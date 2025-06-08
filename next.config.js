4aceeb21d4b8869021226c730abca358b839ab07
tomo <tomomasa0108@gmail.com>
2025-06-09 00:59:17 +0900

    Temporary: Remove PWA dependencies to isolate deployment issue

--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -30,4 +11,4 @@ const nextConfig = {
   }
 }
 
-module.exports = withPWA(nextConfig) 
+module.exports = nextConfig 
