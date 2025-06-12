96a447599a8722f95497a6d9dbcd7ec837c37121
tomo <tomomasa0108@gmail.com>
2025-06-12 23:39:11 +0900

    404エラー解決のためのアプローチ

--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
